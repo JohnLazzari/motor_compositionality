4eafc63facc1b9643987c196655b831035ad6d16
John Lazzari <lazzarijohn2@gmail.com>
2025-04-07 17:53:16 -0400

    beginning to make center out task

--- envs.py
+++ envs.py
@@ -449,3 +449,154 @@ class Maze(env.Environment):
             "goal": self.hidden_goal if self.differentiable else self.detach(self.hidden_goal),
         }
         return obs, info
+
+
+class CenterOutReach(env.Environment):
+    """A reach to a random target from a random starting position.
+
+    Args:
+        network: :class:`motornet.nets.layers.Network` object class or subclass. This is the network that will perform
+        the task.
+        name: `String`, the name of the task object instance.
+        deriv_weight: `Float`, the weight of the muscle activation's derivative contribution to the default muscle L2
+        loss.
+        **kwargs: This is passed as-is to the parent :class:`Task` class.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.obs_noise[:self.skeleton.space_dim] = [0.] * self.skeleton.space_dim  # target info is noiseless
+        self.hidden_goal = None
+        # timestep info
+        self.max_ep_duration = 1
+        self.dt = 0.01
+        self.go_cue = None
+
+    def get_obs(self, t, action=None, deterministic: bool = False) -> th.Tensor | np.ndarray:
+        """
+        Returns a `(batch_size, n_features)` `tensor` containing the (potientially time-delayed) observations.
+        By default, this is the task goal, followed by the output of the :meth:`get_proprioception()` method, 
+        the output of the :meth:`get_vision()` method, and finally the last :attr:`action_frame_stacking` action sets,
+        if a non-zero `action_frame_stacking` keyword argument was passed at initialization of this class instance.
+        `.i.i.d.` Gaussian noise is added to each element in the `tensor`,
+        using the :attr:`obs_noise` attribute.
+        """
+        self.update_obs_buffer(action=action)
+
+        obs_as_list = [
+        self.go_cue[:, t].unsqueeze(1),
+        self.goal,
+        self.obs_buffer["vision"][0],
+        self.obs_buffer["proprioception"][0],
+        ] + self.obs_buffer["action"][:self.action_frame_stacking]
+        
+        obs = th.cat(obs_as_list, dim=-1)
+
+        if deterministic is False:
+            obs = self.apply_noise(obs, noise=self.obs_noise)
+
+        return obs if self.differentiable else self.detach(obs)
+
+    def step(
+        self,
+        t,
+        action: th.Tensor | np.ndarray,
+        **kwargs,
+    ) -> tuple[th.Tensor | np.ndarray, bool, bool, dict[str, Any]]:
+        """
+        Perform one simulation step. This method is likely to be overwritten by any subclass to implement user-defined 
+        computations, such as reward value calculation for reinforcement learning, custom truncation or termination
+        conditions, or time-varying goals.
+        
+        Args:
+        action: `Tensor` or `numpy.ndarray`, the input drive to the actuators.
+        deterministic: `Boolean`, whether observation, action, proprioception, and vision noise are applied.
+        **kwargs: This is passed as-is to the :meth:`motornet.effector.Effector.step()` call. This is maily useful to pass
+        `endpoint_load` or `joint_load` kwargs.
+    
+        Returns:
+        - The observation vector as `tensor` or `numpy.ndarray`, if the :class:`Environment` is set as differentiable or 
+            not, respectively. It has dimensionality `(batch_size, n_features)`.
+        - A `numpy.ndarray` with the reward information for the step, with dimensionality `(batch_size, 1)`. This is 
+            `None` if the :class:`Environment` is set as differentiable. By default this always returns `0.` in the 
+            :class:`Environment`.
+        - A `boolean` indicating if the simulation has been terminated or truncated. If the :class:`Environment` is set as
+            differentiable, this returns `True` when the simulation time reaches `max_ep_duration` provided at 
+            initialization.
+        - A `boolean` indicating if the simulation has been truncated early or not. This always returns `False` if the
+            :class:`Environment` is set as differentiable.
+        - A `dictionary` containing this step's information.
+        """
+        
+        self.elapsed += self.dt
+
+        action = action if th.is_tensor(action) else th.tensor(action, dtype=th.float32).to(self.device)
+        noisy_action = action
+        
+        self.effector.step(noisy_action, **kwargs)
+
+        obs = self.get_obs(t, action=noisy_action)
+        reward = None if self.differentiable else np.zeros((self.detach(action.shape[0]), 1))
+        terminated = bool(self.elapsed >= self.max_ep_duration)
+        self.goal = self.goal.clone()
+        self.hidden_goal = self.goal.clone()
+        info = {
+            "states": self._maybe_detach_states(),
+            "action": action,
+            "noisy action": noisy_action,
+            "goal": self.hidden_goal if self.differentiable else self.detach(self.hidden_goal),
+        }
+
+        return obs, reward, terminated, info
+
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[Any, dict[str, Any]]:
+        """
+        Uses the :meth:`Environment.reset()` method of the parent class :class:`Environment` that can be overwritten to 
+        change the returned data. Here the goals (`i.e.`, the targets) are drawn from a random uniform distribution across
+        the full joint space.
+        """
+        self._set_generator(seed=seed)
+
+        options = {} if options is None else options
+        batch_size: int = options.get('batch_size', 1)
+        reach_conds = options.get('reach_conds', None)
+        joint_state = th.tensor([self.effector.pos_range_bound[0] * 0.5 + self.effector.pos_upper_bound[0] + 0.1, 
+                                self.effector.pos_range_bound[1] * 0.5 + self.effector.pos_upper_bound[1] + 0.5, 0, 0
+        ]).unsqueeze(0).repeat(batch_size, 1)
+        deterministic: bool = options.get('deterministic', False)
+        
+        self.effector.reset(options={"batch_size": batch_size, "joint_state": joint_state})
+
+        self.go_cue = th.ones(size=(batch_size, int(1 / self.dt)))
+
+        fingertip = self.joint2cartesian(joint_state).chunk(2, dim=-1)[0]
+        # Generate 8 equally spaced angles
+        angles = th.linspace(0, 2 * np.pi, 8)
+        # Compute (x, y) coordinates for each angle
+        points = th.stack([th.tensor([np.cos(angle), np.sin(angle)]) for angle in angles], dim=0)
+        # this wont work yet cause everything else has shape batch_size (or I can assert reach_conds and batch_size are same shape)
+        if reach_conds is None:
+            point_idx = th.randint(0, points.size(0), (batch_size,))
+        else:
+            # tensor that will specify which of the 8 conditions to get
+            point_idx = reach_conds
+        self.goal = points[point_idx] * 0.25 + fingertip
+        self.hidden_goal = self.goal.clone()
+        self.elapsed = 0.
+
+        action = th.zeros((batch_size, self.action_space.shape[0])).to(self.device)
+
+        self.obs_buffer["proprioception"] = [self.get_proprioception()] * len(self.obs_buffer["proprioception"])
+        self.obs_buffer["vision"] = [self.get_vision()] * len(self.obs_buffer["vision"])
+        self.obs_buffer["action"] = [action] * self.action_frame_stacking
+
+        action = action if self.differentiable else self.detach(action)
+
+        obs = self.get_obs(0, deterministic=deterministic)
+        info = {
+            "states": self._maybe_detach_states(),
+            "action": action,
+            "noisy action": action,
+            "goal": self.hidden_goal if self.differentiable else self.detach(self.hidden_goal),
+        }
+        return obs, info
--- experiments.py
+++ experiments.py
@@ -4,7 +4,7 @@ from model import Policy
 import torch
 import os
 from utils import load_hp, create_dir, save_fig
-from envs import RandomReach, DlyRandomReach, Maze
+from envs import RandomReach, DlyRandomReach, Maze, CenterOutReach
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
@@ -13,7 +13,7 @@ def train_2link_multi(config_path, model_path, model_file):
     # leave hp as default
     train_2link(config_path, model_path, model_file)
 
-def _test(config_path, model_path, model_file, batch_size=64, joint_state=None, env="RandomReach"):
+def _test(config_path, model_path, model_file, batch_size=256, joint_state=None, env="RandomReach"):
     """ Function will save all relevant data from a test run of a given env
 
     Args:
@@ -37,6 +37,8 @@ def _test(config_path, model_path, model_file, batch_size=64, joint_state=None, 
         env = DlyRandomReach(effector=effector)
     elif env == "Maze":
         env = Maze(effector=effector)
+    elif env == "CenterOutReach":
+        env = CenterOutReach(effector=effector)
 
     # Loading in model
     policy = Policy(
@@ -58,8 +60,9 @@ def _test(config_path, model_path, model_file, batch_size=64, joint_state=None, 
     h = torch.zeros(size=(hp["batch_size"], policy.mrnn.total_num_units))
     
     if joint_state == "center":
-        joint_state = torch.tensor([(effector.pos_lower_bound[0] + effector.pos_upper_bound[0]) / 2, 
-                                    (effector.pos_lower_bound[1] + effector.pos_upper_bound[1]) / 2, 0, 0]).unsqueeze(0).repeat(hp["batch_size"], 1)
+        joint_state = torch.tensor([effector.pos_range_bound[0] * 0.5 + effector.pos_upper_bound[0] + 0.1, 
+                                    effector.pos_range_bound[1] * 0.5 + effector.pos_upper_bound[1] + 0.5, 0, 0
+        ]).unsqueeze(0).repeat(hp["batch_size"], 1)
 
     options = {"batch_size": hp["batch_size"], "joint_state": joint_state}
 
@@ -92,6 +95,28 @@ def _test(config_path, model_path, model_file, batch_size=64, joint_state=None, 
         trial_data[key] = torch.cat(trial_data[key], dim=1)
 
     return trial_data
+
+def center_out(config_path, model_path, model_file, exp_path):
+
+    create_dir(exp_path)
+
+    trial_data = _test(config_path, model_path, model_file, env="CenterOutReach")
+    
+    # Get kinematics and activity in a center out setting
+    # On random and delay
+    colors = plt.cm.inferno(np.linspace(0, 1, trial_data["xy"].shape[0])) 
+
+    for i, (tg, xy) in enumerate(zip(trial_data["tg"], trial_data["xy"])):
+        color = colors[i]
+        plt.plot(xy[:, 0], xy[:, 1], color=color)
+        plt.scatter(xy[0, 0], xy[0, 1], s=150, marker='x', color=color)
+        plt.scatter(tg[:, 0], tg[:, 1], s=150, marker='^', color=color)
+    save_fig(os.path.join(exp_path, "kinematics.png"))
+
+    for i, act in enumerate(trial_data["h"]):
+        color = colors[i]
+        plt.plot(torch.mean(act, dim=-1), color=color)
+    save_fig(os.path.join(exp_path, "psth.png"))
 
 def center_out_random(config_path, model_path, model_file, exp_path):
 
@@ -177,7 +202,7 @@ if __name__ == "__main__":
     parser.add_argument("--config_path", type=str, default="configurations/mrnn.json")
     parser.add_argument("--model_path", type=str, default="checkpoints/mrnn")
     parser.add_argument("--model_file", type=str, default="mrnn.pth")
-    parser.add_argument("--exp_path", type=str, default="results/center_out_random")
+    parser.add_argument("--exp_path", type=str, default="results/mrnn/center_out_random")
     parser.add_argument("--experiment", type=str, default="train_2link_multi")
 
     # Parse the command-line arguments
@@ -191,3 +216,5 @@ if __name__ == "__main__":
         center_out_dlyrandom(args.config_path, args.model_path, args.model_file, args.exp_path) 
     elif args.experiment == "center_out_maze":
         center_out_maze(args.config_path, args.model_path, args.model_file, args.exp_path) 
+    elif args.experiment == "center_out":
+        center_out(args.config_path, args.model_path, args.model_file, args.exp_path) 
